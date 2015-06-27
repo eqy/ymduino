59775c447211639f4a9352e3cafd65e25fe44654
Eddie Yan <eddieyan101@gmail.com>
2015-06-27 13:35:40 -0700

    support streaming raw frames from SD card, older y3/y3 format, visualizations

--- decoder.py
+++ decoder.py
@@ -4,6 +4,8 @@ import argparse
 import array
 import time
 import serial
+import struct
+import math
 
 def readWord():
     char = f.read(1)
@@ -14,31 +16,45 @@ def readWord():
 
 parser = argparse.ArgumentParser(description='path to file')
 parser.add_argument('path')
+parser.add_argument('force_interleaved')
 args = parser.parse_args()
 print("Attempting to read file at %s..."% args.path)
 
 f = open(args.path, 'rb')
 format_type = (f.read(4)).decode('ascii')
+old_format = False
+reg_count = 16
 if format_type == 'YM5!':
     print("YM5! format file detected based on first four bytes of \
 file...")
+elif format_type == 'YM3!' or format_type == 'YM3b':
+    old_format = True
 else:
     print("Unexpected format detected")
 
-f.seek(0x13)
-interleaved = f.read(1)
-if interleaved[0] == 1:
-    print("File is interleaved...")
-else:
-    print("File is not interleaved...")
-f.seek(0x22)
+if not old_format:
+    f.seek(0xc)
+    n_frames = struct.unpack('>i', f.read(4))[0]
+    print(n_frames)
 
-sys.stdout.write("Song title: ")
-readWord()
-sys.stdout.write("Author: ")
-readWord()
-sys.stdout.write("Comments: ")
-readWord()
+    f.seek(0x13)
+    interleaved = f.read(1)
+    if interleaved[0] == 1:
+        print("File is interleaved...")
+    else:
+        print("File is not interleaved...")
+    f.seek(0x22)
+
+    sys.stdout.write("Song title: ")
+    readWord()
+    sys.stdout.write("Author: ")
+    readWord()
+    sys.stdout.write("Comments: ")
+    readWord()
+else:
+    n_frames = math.floor((os.path.getsize(args.path)-4)/14)
+    interleaved = [1]
+    reg_count = 14
 
 byte = f.read(1)
 song_data = []
@@ -50,36 +66,64 @@ for i in range(0,4):
     song_data.pop()
 
 song_data_len = len(song_data)
-n_frames = song_data_len/16.0
+#n_frames = song_data_len/16.0
 
 print("Data length is %i bytes..." %song_data_len)
 print("Song length is %d frames..." %n_frames)
 
-if interleaved[0] ==1:
+registers = {}
+for i in range(0,reg_count):
+    registers[i] = [] 
+if interleaved[0] == 1 or args.force_interleaved == 'force_interleaved':
     print("Deinterleaving...")
-    registers = {}
-    for i in range(0,16):
-        registers[i] = [] 
-        j=0
-        while j < n_frames:
-            registers[i].append(song_data.pop(0))
-            j+=1
+    for i in range(0,reg_count):
+        #j=0
+        registers[i]=song_data[i*n_frames:(i+1)*n_frames]
+        #while j < n_frames:
+        #    try:
+        #        registers[i].append(song_data.pop(0))
+        #        j+=1
+        #    except IndexError:
+        #        print("Couldn't parse end of file, doing best effort playback")
+        #        break
+else:
+    j=0
+    while j < n_frames:
+        for i in range(0,reg_count):
+            try:
+                registers[i].append(song_data.pop(0))
+            except IndexError:
+                print("Couldn't parse end of file, doing best effort playback")
+                j = n_frames
+                break
+        j+=1 
 
-arduino = serial.Serial('/dev/ttyACM0', 9600)
+#arduino = serial.Serial('/dev/ttyACM0', 9600)
 current_time = time.time()
 j=0
+all_frames = array.array('B')
 while j < n_frames:
-    current_time = time.time()
-    frame = array.array('B')
-    j+=1
-    for i in range(0,16):
-        current_reg = registers[i].pop(0)
-        frame.append(current_reg)
-        print("Register {reg_n:>2d}: 0x{value:02x} {bin_value:08b}".format( \
-            reg_n=i, value=current_reg, bin_value=current_reg))
-    arduino.write(frame)
-    print("{:=^26}".format(""))
-    time.sleep(0.01)         
+    try:
+        current_time = time.time()
+        frame = array.array('B')
+        for i in range(0,14):
+            current_reg = registers[i].pop(0)
+            frame.append(current_reg)
+            all_frames.append(current_reg)
+            print("Register {reg_n:>2d}: 0x{value:02x} {bin_value:08b}".format( \
+                reg_n=i, value=current_reg, bin_value=current_reg))
+        #arduino.write(frame)
+        print(\
+        "==Frame#{fr:05d}/{tot:05d}======".format(fr=int(j),tot=int(n_frames-1)))
+        #time.sleep(0.02)         
+        j+=1
+    except IndexError:
+        print("reached end of frames early...")
+        break
+
+with open('outputfile', 'wb') as f:
+    all_frames.tofile(f)
+
 
 
 
--- ymduino.ino
+++ ymduino.ino
@@ -2,17 +2,36 @@
 #define DA_1 5
 #define DA_2 6
 #define DA_3 7
-#define DA_4 8
-#define DA_5 9
-#define DA_6 10
-#define DA_7 11
+//#define DA_4 8
+//#define DA_5 9
+//#define DA_6 10
+//#define DA_7 11
+#define DA_4 19
+#define DA_5 18
+#define DA_6 17
+#define DA_7 16
+
+
+#define CS   10
+
+#define SDATA 8
+#define RCLK  14
+#define SRCLK 15
+
 
 #define INACTIVE B11110011
 #define READ     B00000100
 #define WRITE    B00001000
 #define ADDRESS  B00001100
+#define CYCLE_TIME 20000
+
+
+#include <SD.h>
 
 void writeRegister(int reg, int* frame);
+
+File root;
+File current_song;
 
 void setup()
 {
@@ -21,8 +40,9 @@ void setup()
   DDRD = DDRD | B00001100;
   /*Set the remaining pins used for output in the conventional way
   */
-  //pinMode(2, OUTPUT);
-  //pinMode(3, OUTPUT);
+  pinMode(SDATA, OUTPUT);
+  pinMode(RCLK, OUTPUT);
+  pinMode(SRCLK, OUTPUT);
   pinMode(DA_0, OUTPUT);
   pinMode(DA_1, OUTPUT);
   pinMode(DA_2, OUTPUT);
@@ -31,38 +51,136 @@ void setup()
   pinMode(DA_5, OUTPUT);
   pinMode(DA_6, OUTPUT);
   pinMode(DA_7, OUTPUT);
+  
+  pinMode(CS, OUTPUT);
 
+  digitalWrite(RCLK, LOW);
+  digitalWrite(SRCLK, LOW);
   Serial.begin(9600);
+  
+  if (!SD.begin(CS))
+  {
+     Serial.println("Initialization Failed");
+  }
+  else
+  {
+     Serial.println("Initialization OK");
+     root = SD.open("/");
+     current_song = root.openNextFile();
+     if (current_song)
+     {
+         Serial.println("File Open OK");
+     }
+  }
+     
+  int i = 0;
+  for (i = 0; i < 8; i++)
+  {
+      writeShift(0x80 >> i);
+      delay(50); 
+  }
+  for (i = 0; i < 8; i++)
+  {
+      writeShift(0x01 << i);
+      delay(50); 
+  }
   PORTD &= INACTIVE;
+
+}
+
+void writeShift(byte val)
+{
+    digitalWrite(SRCLK, LOW);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SDATA, val & 0x1);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SDATA, (val & 0x2) >> 1);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SDATA, (val & 0x4) >> 2);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SDATA, (val & 0x8) >> 3);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SDATA, (val & 0x10) >> 4);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SDATA, (val & 0x20) >> 5);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SDATA, (val & 0x40) >> 6);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SDATA, (val & 0x80) >> 7);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+    digitalWrite(SRCLK, HIGH);
+    digitalWrite(SRCLK, LOW);
 }
 
 void loop()
 {
-  int frame[16];
-  if(Serial.available() >= 16)
+  //int frame[16];
+  unsigned long start = micros();      
+  if(current_song.available())
   {
-      int i;
-      for(i = 0; i < 16; i++)
-      {
-        frame[i] = Serial.read();
-      }
-          
-      int j;
-      for(j = 0; j < 16; j++)
-        writeRegister(j, frame);   
+      //int i;
 
-      /*for(j = 0; j < 16; j++)
-      {
-        Serial.print(frame[j], HEX);
-        Serial.print(' ');
-
-      }*/
+      byte reg_0 = current_song.read();
+      byte reg_1 = current_song.read();
+      byte reg_2 = current_song.read();
+      byte reg_3 = current_song.read();
+      byte reg_4 = current_song.read();
+      byte reg_5 = current_song.read();
+      byte reg_6 = current_song.read();
+      byte reg_7 = current_song.read();
+      byte reg_8 = current_song.read();
+      byte reg_9 = current_song.read();
+      byte reg_10 = current_song.read();
+      byte reg_11 = current_song.read();
+      byte reg_12 = current_song.read();
+      byte reg_13 = current_song.read();
+      //byte reg_14 = current_song.read();
+      //byte reg_15 = current_song.read();
       
+      //for(i = 0; i < 16; i++)
+      //{
+      //  frame[i] = Serial.read();
+      //}
+      writeRegister(0, reg_0);   
+      writeRegister(1, reg_1);   
+      writeRegister(2, reg_2);   
+      writeRegister(3, reg_3);   
+      writeRegister(4, reg_4);   
+      writeRegister(5, reg_5);   
+      writeRegister(6, reg_6);   
+      writeRegister(7, reg_7);   
+      writeRegister(8, reg_8);   
+      writeRegister(9, reg_9);   
+      writeRegister(10, reg_10);   
+      writeRegister(11, reg_11);   
+      writeRegister(12, reg_12);   
+      writeRegister(13, reg_13);   
+      writeShift(reg_0);
+
+      while(micros() - start < CYCLE_TIME);
        //Serial.print("\n");
+  }
+  else
+  {
+      current_song = root.openNextFile();
+      if (!current_song)
+      {
+          root.rewindDirectory();
+          current_song = root.openNextFile(); 
+      }
   }
 }  
 
-void writeRegister(int reg, int* frame)
+void writeRegister(byte reg, byte val)
 {
       //int j;
       //  Serial.print(frame[reg], HEX);
@@ -78,21 +196,21 @@ void writeRegister(int reg, int* frame)
       digitalWrite(DA_5, 0);
       digitalWrite(DA_6, 0);
       digitalWrite(DA_7, 0);
-      delayMicroseconds(2);
+      delayMicroseconds(1);
       PORTD &= INACTIVE;
-      digitalWrite(DA_0, frame[reg] & 0x1);
-      digitalWrite(DA_1, (frame[reg] & 0x2) >> 1);
-      digitalWrite(DA_2, (frame[reg] & 0x4) >> 2);
-      digitalWrite(DA_3, (frame[reg] & 0x8) >> 3);
-      digitalWrite(DA_4, (frame[reg] & 0x10) >> 4);
-      digitalWrite(DA_5, (frame[reg] & 0x20) >> 5);
-      digitalWrite(DA_6, (frame[reg] & 0x40) >> 6);
-      digitalWrite(DA_7, (frame[reg] & 0x80) >> 7);
-      delayMicroseconds(15);
+      digitalWrite(DA_0, val & 0x1);
+      digitalWrite(DA_1, (val & 0x2) >> 1);
+      digitalWrite(DA_2, (val & 0x4) >> 2);
+      digitalWrite(DA_3, (val & 0x8) >> 3);
+      digitalWrite(DA_4, (val & 0x10) >> 4);
+      digitalWrite(DA_5, (val & 0x20) >> 5);
+      digitalWrite(DA_6, (val & 0x40) >> 6);
+      digitalWrite(DA_7, (val & 0x80) >> 7);
+      delayMicroseconds(1);
       PORTD |= WRITE;
-      delayMicroseconds(15);
+      delayMicroseconds(1);
       PORTD &= INACTIVE;
-      //delayMicroseconds(5);
 }
+
 
 
